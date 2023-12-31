34b635f7ed99aa24843ebb1f1d7d4e5398a9a840
popsterrs <jackpadbury1@icloud.com>
2023-12-31 19:25:01 +0000

    update config variables
    use my own tmdb api key and proxy

--- public/config.js
+++ public/config.js
@@ -1,9 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "CHANGEME",
+  // VITE_CORS_PROXY_URL: "https://cors-anywhere.herokuapp.com",
+  VITE_CORS_PROXY_URL: "simple-proxy.jackpadbury1.workers.dev",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTBkNTZlMDQxZmYzY2M3Njk2NTZiMWRjOWUyNjYwNiIsInN1YiI6IjY0MjliYmI2NjY1NDA4MDA5N2Y1ZjU2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.89b7RF2I-Dm0y_b2_2j6krUKFDgwN0jvn4RAbRy616w",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
