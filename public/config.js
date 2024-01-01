4ab2639e8e6b50d15bf2239ae1e29d22dbf633bc
popsterrs <jackpadbury1@icloud.com>
2024-01-01 16:50:04 +0000

    c
    c

--- public/config.js
+++ public/config.js
@@ -1,4 +1,5 @@
 window.__CONFIG__ = {
+<<<<<<< HEAD
     // The URL for the CORS proxy, the URL must NOT end with a slash!
     VITE_CORS_PROXY_URL: process.env.VITE_CORS_PROXY_URL,
   
@@ -18,3 +19,24 @@ window.__CONFIG__ = {
     VITE_DISALLOWED_IDS: ""
   };
 
+=======
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  // VITE_CORS_PROXY_URL: "https://cors-anywhere.herokuapp.com",
+  VITE_CORS_PROXY_URL: "simple-proxy.jackpadbury1.workers.dev",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTBkNTZlMDQxZmYzY2M3Njk2NTZiMWRjOWUyNjYwNiIsInN1YiI6IjY0MjliYmI2NjY1NDA4MDA5N2Y1ZjU2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.89b7RF2I-Dm0y_b2_2j6krUKFDgwN0jvn4RAbRy616w",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
+  VITE_BACKEND_URL: null,
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: ""
+};
+>>>>>>> parent of fbca369 (Merge branch 'dev' of https://github.com/popsterrs/movie-web into dev)
