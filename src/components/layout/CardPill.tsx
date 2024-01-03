235c72b5c512e720b00bb7569d09a3042a199120
popsterrs <jackpadbury1@icloud.com>
2024-01-03 19:44:50 +0000

    updated padding and spacing for navigation bar

--- src/components/layout/CardPill.tsx
+++ src/components/layout/CardPill.tsx
@@ -7,11 +7,10 @@ export function CardPill(props: {
   backgroundClass?: string;
   children: React.ReactNode;
 }) {
-
   return (
     <div
       className={classNames(
-        "flex items-center space-x-2 rounded-full px-4 py-2 text-type-logo",
+        "flex items-center space-x-6 rounded-full px-8 py-2 text-type-logo",
         props.backgroundClass ?? "bg-pill-background bg-opacity-50",
         props.clickable
           ? "transition-[transform,background-color] hover:scale-105 hover:bg-pill-backgroundHover hover:text-type-logo active:scale-95"
@@ -19,7 +18,7 @@ export function CardPill(props: {
         props.hideCardOnMobile ? "hidden sm:block" : "",
       )}
     >
-    {props.children}
+      {props.children}
     </div>
   );
 }
